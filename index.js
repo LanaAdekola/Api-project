e5d0c21630725c1a28ec9a328643a91217f5f51c
LanaAdekola <adekolaadelana@gmail.com>
2023-03-23 09:40:07 +0100

    .

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
+let postArray = []
+
+function renderPosts(){
+    let html = ""
+    for (let post of postArray){
+        html += `
+        <h1>${post.title}</h1>
+        <p>${post.body}</p>
+        `
+    }
+    document.getElementById('blog-list').innerHTML = html
+}
 
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        postArray = data.slice(0, 5)
+        renderPosts()
+    })
+    
+document.getElementById('btn').addEventListener('click', function(e){
+    e.preventDefault()
+    let postTitle = document.getElementById('post-title').value
+    let postBody = document.getElementById('post-body').value
+    let data =  {
+            title: postTitle,
+            body: postBody
+        }
 
+const options =  {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+        "Content-type" : "application/json"
+    }
+}
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then(res => res.json())
+    .then(post => {
+        postArray.unshift(post)
+        renderPosts()
+    })
 
-
+})
